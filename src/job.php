28567f672768c5e6c2cd47737dab445be6a4035e
Remco Wolvenne <remcowolvenne@Remcos-MacBook-Air.local>
2024-06-11 13:40:15 +0200

    Initial commit

---
+++ src/job.php
@@ -0,0 +1,99 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+$old = json_decode(file_get_contents('current_ip.json'));
+
+$client = new GuzzleHttp\Client();
+
+$cfAuthHeaders = [
+    'X-Auth-Email' => getenv('AUTH_EMAIL'),
+    'X-Auth-Key' => getenv('AUTH_KEY'),
+];
+
+# Check current IP
+$response = $client->get('checkip.amazonaws.com');
+$currentIp = preg_replace('/\s+/', '', $response->getBody());
+
+echo "Current IP is {$currentIp}." . PHP_EOL;
+
+# First run, or when IP address is changed
+if ($old->ip != $currentIp) {
+
+    echo "This is the fist run or the IP is changed." . PHP_EOL;
+
+    # If there is an old IP, delete it first
+    if ($old->id) {
+        try {
+            $client->delete('https://api.cloudflare.com/client/v4/accounts/6e05adcb1ece5983dd0a6e7c29ba0f1a/rules/lists/80d4cf20a595453fbd96a3d6f6329e91/items', [
+                'headers' => $cfAuthHeaders + [
+                    'Content-Type' => 'application/json',
+                ],
+                'json' => [
+                    'items' => [
+                        ['id' => $old->id],
+                    ],
+                ],
+            ]);
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            echo $e->getMessage();
+        }
+
+        echo "Deleted old IP {$old->ip} with ID {$old->id}." . PHP_EOL;
+    }
+
+    # Create new IP item
+    $client->post('https://api.cloudflare.com/client/v4/accounts/6e05adcb1ece5983dd0a6e7c29ba0f1a/rules/lists/80d4cf20a595453fbd96a3d6f6329e91/items', [
+        'headers' => $cfAuthHeaders + [
+            'Content-Type' => 'application/json',
+        ],
+        'json' => [
+            ['ip' => $currentIp],
+        ],
+    ]);
+
+    echo "Created new IP item in Cloudflare." . PHP_EOL;
+
+    # Get ID of newly created IP. Since this is an asynchronous action within Cloudflare, it may not be processed yet. So try max 10 times.
+    $currentId = null;
+
+    for ($i = 10; $i > 0; $i--) {
+          
+        $response = $client->get('https://api.cloudflare.com/client/v4/accounts/6e05adcb1ece5983dd0a6e7c29ba0f1a/rules/lists/80d4cf20a595453fbd96a3d6f6329e91/items', [
+            'headers' => $cfAuthHeaders,
+        ]);
+        
+        $body = json_decode($response->getBody());
+        
+        foreach ($body->result as $item) {
+            if ($item->ip == $currentIp) {
+                $currentId = $item->id;
+                break;
+            }
+        }
+
+        if ($currentId) {
+            echo "ID {$currentId} found of newly created IP." . PHP_EOL;
+            break;
+        }
+
+        echo "ID of new IP not yet found, try again in 3 seconds." . PHP_EOL;
+
+        sleep(3);
+    }
+
+    if (!$currentId) {
+        throw new Exception('After 10 tries, unable to get the ID of the newly created IP.');
+    }
+
+    # Save to file
+    file_put_contents('current_ip.json', json_encode([
+        'id' => $currentId,
+        'ip' => $currentIp,
+    ]));
+
+    echo "Saved new IP {$currentIp} with ID {$currentId}" . PHP_EOL;
+
+} else {
+    echo "Nothing is changed, so do nothing." . PHP_EOL;
+}
---
+++ src/readme.md
@@ -0,0 +1,39 @@
+# Setup Cloudflare
+
+## 1. Create a list
+1. Go to https://dash.cloudflare.com
+2. Manage account -> Configurations -> Lists
+3. Create new list:
+    * Identifier: public_ips
+
+## 2. Create a WAF rule
+1. Go to https://dash.cloudflare.com
+2. Websites -> Domain -> Security -> WAF
+3. Create new rule:
+    * Rule name: Block when the IP address is not ours
+    * Action: Block
+    * Expression: 
+        ```
+        (http.host eq "sub.domain.com" and not ip.src in $public_ips)
+        ```
+
+# Docker
+
+## 1. Prerequisites
+1. Copy `docker-compose.example.yml` to `docker-compose.yml`
+2. Edit `docker-compose.yml`
+
+## 2. Build (optional)
+```bash 
+docker compose build
+```
+
+## 3. Deploy
+```bash 
+docker compose up -d
+```
+
+# Run job manually
+```bash
+docker compose exec ip php job.php
+```
